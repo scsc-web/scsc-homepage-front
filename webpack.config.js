f3f98a6b08c5fdb1cbf8673fb9a18663becb3113
Myeongseop Jeon <jeonms1611@gmail.com>
2018-08-01 19:33:24 +0900

    Use webpack to load .scss files
    Webpack config file is used for loading .scss files, instead of build
    script. The build script will be removed.

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,40 @@
 /* eslint-env node */
 
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: './src/scripts/index.js',
+  entry: {
+    index: './src/scripts/index.js',
+    login: './src/scripts/login.js'
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js'
+    filename: '[name].js'
   },
   plugins: [
+    new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
       template: 'src/index.html',
-      filename: 'index.html'
+      filename: 'index.html',
+      chunks: ['index']
     }),
     new HtmlWebpackPlugin({
       template: 'src/login.html',
-      filename: 'login.html'
+      filename: 'login.html',
+      chunks: ['login']
     })
-  ]
+  ],
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        'css-loader',
+        'sass-loader'
+      ]
+    }]
+  }
 }
